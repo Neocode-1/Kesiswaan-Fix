c68d67780e8ad555741b0330ffd1846d80669a04
Neocode-1 <neocode.g1@gmail.com>
2025-04-16 21:52:02 +0700

    feature: membuat controller siswa

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::resource('/user', UserController::class);
+Route::resource('/siswa', SiswaController::class);
