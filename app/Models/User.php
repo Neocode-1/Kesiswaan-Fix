4bedb738ac04720411d5ab4f2a69e87d9e1ba9c9
Salmoon16 <alfarizijunior123@gmail.com>
2025-04-16 10:12:17 +0700

    salman : tambah

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,28 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'nama',
+        'nisn' ,
+        'ttl' ,
+        'jenis_kelamin',
+        'agama' ,
+        'sklh_asal' ,
+        'tgl_masuk' ,
+        'tgl_keluar' ,
+        'status_klrga ',
+        'anak_ke' ,
+        'alamat' ,
+        'telp_rumah' ,
+        'status_pip' ,
+        'nama_ortu',
+        'alamat_ortu',
+        'no_telp',
+        'pekerjaan',
+        'nama_wali',
+        'alamat_wali',
+        'pekerjaan_wali',
+        // 'email',
+        // 'password',
     ];
 
     /**
