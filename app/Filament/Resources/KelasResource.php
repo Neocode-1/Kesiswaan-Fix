702266619d7a0e6442b6c02bd599b83b1f6b5b5e
Neocode-1 <neocode.g1@gmail.com>
2025-05-03 14:32:56 +0700

    menambahkan feature:resource-kelas (Perlu ada perbaikan pada bagian form karen pada bagian admin_id tidak muncul di table ketika mencoba untuk edit maupun membuat data baru

--- app/Filament/Resources/KelasResource.php
+++ app/Filament/Resources/KelasResource.php
@@ -6,9 +6,17 @@ use App\Filament\Resources\KelasResource\Pages;
 use App\Filament\Resources\KelasResource\RelationManagers;
 use App\Models\Kelas;
 use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\TextInputColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -17,27 +25,108 @@ class KelasResource extends Resource
 {
     protected static ?string $model = Kelas::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-building-library';
+
+    protected static ?string $navigationGroup = 'Kelas Siswa';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Section::make()
+                    ->columns()
+
+                    ->schema([
+                        TextInput::make('absensi_id')
+                            ->label('Nama Siswa')
+                            ->placeholder('Masukan nama siswa')
+                            ->required(),
+
+                        Select::make('tingkat')
+                            ->label('Tingkat')
+                            ->options([
+                                'x' => 'X',
+                                'xi' => 'XI',
+                                // 'xii' => 'XII'
+                            ])
+                            ->native(false)
+                            ->searchable()
+                            ->placeholder('masukan tingkat siswa')
+                            ->required(),
+
+                        TextInput::make('kebutuhan')
+                            ->label('Siswa ber kebutuhan')
+                            ->placeholder('masukan keterangan nya')
+                            ->required(),
+
+                        Select::make('nama_kelas')
+                            ->label('Nama kelas')
+                            ->required()
+                            ->options([
+                                'a' => 'A',
+                                'b' => 'B',
+                                'c' => 'C',
+                                'd' => 'D'
+                            ])
+                            ->searchable()
+                            ->placeholder('masukan nama kelas')
+                    ])
+
+
             ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
+            // ->poll('5s')
             ->columns([
-                //
+                TextColumn::make('absensi.admin.name')
+                    ->label('Nama Siswa')
+                    ->icon('heroicon-o-pencil-square')
+                    ->iconColor('primary')
+                    ->searchable(),
+
+                TextColumn::make('tingkat')
+                    ->label('Tingkatan')
+                    ->icon('heroicon-o-arrow-up-tray')
+                    ->iconColor('primary')
+                    ->searchable(),
+
+                TextColumn::make('nama_kelas')
+                    ->label('Nama kelas')
+                    ->copyable()
+                    ->copyMessage('nama kelas berhasil di salin')
+                    ->icon('heroicon-o-arrow-right-end-on-rectangle')
+                    ->badge()
+
+                    ->searchable(),
+
+                TextColumn::make('kebutuhan')
+                    ->label('kebutuhan siswa')
+                    ->icon('heroicon-o-cube-transparent')
+                    ->iconColor('primary')
+                    ->badge()
+                    ->searchable(),
+
+
             ])
             ->filters([
-                //
+                SelectFilter::make('tingkat')
+                    ->label('Tingkat')
+                    ->options([
+                        'X' => 'X',
+                        'XI' => 'XI',
+                        'XII' => 'XII'
+                    ])
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\EditAction::make()->tooltip('Edit'),
+                    Tables\Actions\ViewAction::make()->tooltip('View'),
+                    Tables\Actions\DeleteAction::make()->tooltip('Delete'),
+
+                ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -60,5 +149,17 @@ class KelasResource extends Resource
             'create' => Pages\CreateKelas::route('/create'),
             'edit' => Pages\EditKelas::route('/{record}/edit'),
         ];
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        $kelasData = Kelas::all()->count();
+        $stringCount = strval($kelasData);
+        return $stringCount;
+    }
+
+    public static function getNavigationBadgeTooltip(): ?string
+    {
+        return 'Total kebutuhan';
     }
 }
--- app/Filament/Resources/KelasResource/Pages/ListKelas.php
+++ app/Filament/Resources/KelasResource/Pages/ListKelas.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\KelasResource\Pages;
 
 use App\Filament\Resources\KelasResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListKelas extends ListRecords
@@ -13,7 +14,20 @@ class ListKelas extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('Tambah Nama siswa')
+                ->color('primary')
+                ->icon('heroicon-o-user-plus'),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'x' => Tab::make()->query(fn($query) => $query->where('tingkat', 'X')),
+            'xi' => Tab::make()->query(fn($query) => $query->where('tingkat', 'XI')),
+            'xii' => Tab::make()->query(fn($query) => $query->where('tingkat', 'XII')),
+            'All' => Tab::make(),
         ];
     }
 }
